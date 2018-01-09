7586185d65695fa9254f2ea154ebca0c5f7c6a57
CuongNguyen <p1code@prototype1.io>
2018-01-09 15:55:19 +0700

    added button component

---
+++ src/frontend/app/components/input/input.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import classnames from "classnames";
+
+export class Input extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let {label, className, value} = this.props;
+        return (
+            <div className={classnames("ad-input", className)}>
+                <input
+                    {...this.props}
+                />
+                <label className={value && value.length > 0 && "has-value"}>{label}</label>
+                <span className={classnames("bar", value && value.length > 0 && "has-value")} />
+            </div>
+        );
+    }
+}
--- src/frontend/app/main-router.jsx
+++ src/frontend/app/main-router.jsx
@@ -1,25 +1,38 @@
 import React from "react";
 import {Button} from "./components/button/button";
+import {Input} from "./components/input/input";
 export class MainRouter extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            value: "1123"
+        }
     }
 
 
 
     render() {
 
+        let {value} = this.state;
 
         return (
             <div className="main-router">
                 <Button
-                    disabled
                     onClick={(e) => console.log(e.button)}
                     className="btn-blue"
                 >
                     Hello
                 </Button>
+
+                <div style={{width: 300 + 'px', marginTop: '40px'}}>
+                    <Input
+                        label="Name"
+                        placeholder="Please enter your name"
+                        value={value}
+                        onChange={(e) => this.setState({value: e.target.value})}
+                    />
+                </div>
             </div>
         );
     }
