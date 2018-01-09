ada3a6f4c213d3a3237ea2482aebcfd210811c9b
CuongNguyen <p1code@prototype1.io>
2018-01-09 12:17:27 +0700

    added button components

---
+++ src/frontend/app/components/button/button.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import classnames from "classnames";
+
+export class Button extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    mouseDown(e) {
+        let ripple, size, rippleX, rippleY, eWidth, eHeight;
+        let self = $(ReactDOM.findDOMNode(this));
+
+        if(self.find('.ripple').length === 0) {
+            self.prepend('<span class="ripple"></span>');
+        }
+        ripple = self.find('.ripple');
+        ripple.removeClass('animated');
+
+        eWidth = self.outerWidth();
+        eHeight = self.outerHeight();
+        size = Math.max(eWidth, eHeight);
+        ripple.css({'width': size, 'height': size});
+
+        rippleX = parseInt(e.pageX - self.offset().left) - (size / 2);
+        rippleY = parseInt(e.pageY - self.offset().top) - (size / 2);
+
+        ripple.css({ 'top': rippleY +'px', 'left': rippleX +'px' }).addClass('animated');
+
+        setTimeout(function() {
+            ripple.remove();
+        }, 800);
+
+    }
+
+    render() {
+
+        let {className, children} = this.props;
+
+        return (
+            <button
+                onMouseDown={(e) => this.mouseDown(e)}
+                {...this.props}
+                className={classnames("btn ripple-effect", className)}>
+                {children}
+            </button>
+        );
+    }
+}
--- src/frontend/app/main-router.jsx
+++ src/frontend/app/main-router.jsx
@@ -1,54 +1,25 @@
 import React from "react";
-import {modals, ModalsRegistry} from "./components/modal/modals";
+import {Button} from "./components/button/button";
 export class MainRouter extends React.Component {
 
     constructor(props) {
         super(props);
     }
 
-    openModal() {
-        let modal = modals.openModal({
-            content: (
-                <div className="modal-test">
-                    <div className="modal-header">
-                        <div className="modal-title">
-                            Are you sure?
-                        </div>
-                    </div>
 
-                    <div className="modal-body">
-                        Et has debitis vivendo, nam dicant malorum te. Justo moderatius elaboraret cu est, nibh placerat insolens id mea. Suas equidem usu ad, eos ex sint stet alterum. Sit in adhuc propriae contentiones, dicta decore eum an.
-                    </div>
-
-                    <div className="modal-footer">
-                        <button className="btn btn-secondary">
-                            Nope
-                        </button>
-
-                        <button className="btn btn-primary">
-                            Yep
-                        </button>
-                    </div>
-                </div>
-            )
-        })
-
-    }
 
     render() {
 
-        setTimeout(() => {
-            this.openModal();
-
-        }, 1000);
 
         return (
             <div className="main-router">
-                Xin chào
-
-                <ModalsRegistry />
-
-
+                <Button
+                    disabled
+                    onClick={(e) => console.log(e.button)}
+                    className="btn-blue"
+                >
+                    Hello
+                </Button>
             </div>
         );
     }
